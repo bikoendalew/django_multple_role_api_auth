2adcf74dc31c5969afa53be235fe51c872d33df7
biruk <birukendalew090@gmail.com>
2024-12-11 11:44:13 +0300

    add urls

--- Main/urls.py
+++ Main/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
-    path('api/accounts/', include('accounts.urls')),
+    path('api/accounts/', include('User.urls')),
 ]
