6787c0f27133656a4fce8cea0738ff2e27300e0d
biruk <birukendalew090@gmail.com>
2024-12-11 11:33:33 +0300

    add abstractUser to User model

--- User/models.py
+++ User/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('superadmin', 'Super Admin'),
+        ('renter', 'Renter'),
+        ('owner', 'Owner'),
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='renter')
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'  # Set email as the primary identifier
+    REQUIRED_FIELDS = ['username']  # 
